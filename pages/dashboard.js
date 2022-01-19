dc65f823840d7b36c75c8db16f021e536293c382
jewoo <jeus0630@gmail.com>
2022-01-19 11:15:59 -0500

    secure Page / getSession

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,16 @@
-
-
+import {signIn, getSession} from "next-auth/react";
+import {useEffect} from 'react';
 const Dashboard = () => {
+    useEffect(async ()=>{
+        const session = await getSession();
+        if(!session){
+            signIn();
+        }
+    },[]);
+
     return (
         <div>
-            
+            <h1>Dashboard</h1>
         </div>
     );
 };
