a794534cb8823f215b68d13f3301ba1a1a6fd7dd
jewoo <jeus0630@gmail.com>
2022-01-19 10:37:16 -0500

    signIn / signOut

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Link from "next/link";
+import {signIn, signOut} from "next-auth/react";
+
 const Navbar = () => {
     return (
         <nav>
@@ -17,6 +19,22 @@ const Navbar = () => {
                 <li>
                     <Link href={"/dashboard"}>
                         <a>dashboard</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href={"/api/auth/signin"}>
+                        <a onClick={e=>{
+                            e.preventDefault();
+                            signIn();
+                        }}>Sign In</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href={"/api/auth/signout"}>
+                        <a onClick={e=>{
+                            e.preventDefault();
+                            signOut();
+                        }}>Sign Out</a>
                     </Link>
                 </li>
             </ul>
