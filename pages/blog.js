d5153a1840eb968c919d24899396c1b57d1ced63
jewoo <jeus0630@designfever.com>
2022-01-20 11:42:42 +0900

    serverside auth

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,30 @@
+import {getSession} from "next-auth/react";
 
-
-const Blog = () => {
+const Blog = ({data}) => {
     return (
         <div>
-
+            <h1>Blog page - {data}</h1>
         </div>
     );
 };
+
+export const getServerSideProps = async (context) => {
+    const session = await getSession(context);
+
+    if(!session){
+        return {
+            redirect : {
+                destination : `/api/auth/signin?callbackUrl=http://localhost:3000/blog`,
+                permanent : false
+            }
+        }
+    }
+
+    return {
+        props : {
+            data : session ? ' List of 100 personalized blogs' : 'List of free blogs'
+        }
+    }
+}
 
 export default Blog;
